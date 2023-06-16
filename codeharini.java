115eda11442bda2c2c1b02c881ecf1771fdfd428
JEEVA-001 <113702840+JEEVA-001@users.noreply.github.com>
2023-06-16 14:07:25 +0530

    change jeev to jeeva in 2nd line

--- codeharini.java
+++ codeharini.java
@@ -4,7 +4,8 @@ public class codeharini {
     public static void main(String[] args)
     {
         System.out.println("jeeva");
-        System.out.println("jeev");
+        System.out.println("jeeva");
+        
     }
     
 }
